c1732d19a6a579edd527037e8683999d838d310f
Mirror83 <glenochieng045@gmail.com>
2024-08-02 10:36:54 +0300

    Added selected attribute

--- scenes/Tile.gd
+++ scenes/Tile.gd
@@ -1,2 +1,14 @@
+@tool
 class_name Tile
 extends Sprite2D
+
+@export var selected = false
+
+var tile_size = get_rect().size
+
+func select():
+	selected = true
+	
+func deselect():
+	selected = false
+	

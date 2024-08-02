1692adf280dc1368b4e40e282220fde13c99b259
Mirror83 <glenochieng045@gmail.com>
2024-08-02 10:54:56 +0300

    Added simple focus-rectangle for board tiles

--- scenes/Board.gd
+++ scenes/Board.gd
@@ -1,5 +1,4 @@
 extends Node2D
-enum {}
 
 const FRAME_WIDTH = 12;
 const FRAME_HEIGHT = 9;
@@ -10,24 +9,47 @@ const BOARD_SIZE = 8
 var tile_coords: Array[Array] = [];
 var board: Array[Array];
 
-var selected_tile: Tile;
+var focused_tile_index: Vector2i;
 
 var screen_size;
-
+var tile_size;
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 	tile_coords = _generate_tile_coords()
 	board = _generate_board()
-	
-	selected_tile = board[0][0]
-	var tile_size = selected_tile.get_rect().size
-	
+			
 	position = Vector2(
 		lerpf(0, screen_size.x, 0.5) - (tile_size.x * BOARD_SIZE) / 2, 
 		screen_size.y / 2 - (tile_size.y * BOARD_SIZE) / 2
 	)
+	
+	_focus_on_tile(Vector2i(0, 0))
+	
+func _process(_delta):
+	_manage_tile_focus()
+	
+func _manage_tile_focus():
+	if Input.is_action_just_pressed("right"):
+		focused_tile_index.y += 1 
+		focused_tile_index.y %= BOARD_SIZE
+		_focus_on_tile(focused_tile_index)
+		
+	elif Input.is_action_just_pressed("left"):
+		focused_tile_index.y -= 1 
+		focused_tile_index.y %= BOARD_SIZE
+		_focus_on_tile(focused_tile_index)
+	
+	elif Input.is_action_just_pressed("down"):
+		focused_tile_index.x += 1 
+		focused_tile_index.x %= BOARD_SIZE
+		_focus_on_tile(focused_tile_index)
+		
+	elif Input.is_action_just_pressed("up"):
+		focused_tile_index.x -= 1 
+		focused_tile_index.x %= BOARD_SIZE
+		_focus_on_tile(focused_tile_index)
 
 func _generate_tile_coords():
 	var coords: Array[Array] = []
@@ -46,8 +68,10 @@ func _generate_tile_coords():
 	
 func _generate_board() -> Array[Array]:
 	var tile_board: Array[Array] = []
+	
 	var current_tile = preload("res://scenes/Tile.tscn").instantiate()
-	var tile_size = current_tile.get_rect().size
+	tile_size = current_tile.get_rect().size
+	
 	var tile_position = tile_size / 2
 	
 	for i in range(BOARD_SIZE):
@@ -70,3 +94,14 @@ func _generate_board() -> Array[Array]:
 		
 	return tile_board
 
+func _focus_on_tile(tile_index: Vector2i):
+	var tile = board[tile_index.x][tile_index.y]
+	tile.select()
+	queue_redraw()
+	
+func _get_focused_tile():
+	return board[focused_tile_index.x][focused_tile_index.y]
+	
+func _draw():
+	var focused_tile = _get_focused_tile()
+	draw_rect(Rect2(focused_tile.position - focused_tile.tile_size / 2, tile_size), Color(0, 0, 0), false, 3)

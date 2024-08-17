5871f726e233effc0434b02c28264bbcb46fe937
Mirror83 <glenochieng045@gmail.com>
2024-08-17 11:12:58 +0300

    feat: set up score label to update on match
    Also added other labels and game config variables like level and timer value

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,12 +1,27 @@
 extends Node2D
 class_name Game
 
+signal level_complete;
 
-# Called when the node enters the scene tree for the first time.
+const START_GOAL = 1500
+const GOAL_INCREMENT = 2000
+var goal = 5000
+
+var level = 1
+var timer_value = 0
+
+var total_score = 0
+
 func _ready():
-	pass # Replace with function body.
+	$GameStatus/HBoxContainer/Values/Level.text = "%d" % level
+	$GameStatus/HBoxContainer/Values/Score.text = "%d" % total_score
+	$GameStatus/HBoxContainer/Values/Goal.text = "%d" % goal
+	$GameStatus/HBoxContainer/Values/Timer.text = "%d" % timer_value
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_board_match_found(score):
+	total_score += score
+	$GameStatus/HBoxContainer/Values/Score.text = "%d" % total_score
+	
+	if total_score >= goal:
+		level_complete.emit()
+		

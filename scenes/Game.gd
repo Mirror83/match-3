de6e474ed12e8c2f259d151494c5e4ec5e0c5887
Mirror83 <glenochieng045@gmail.com>
2024-08-18 08:19:08 +0300

    feat: added timer

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -1,14 +1,15 @@
 extends Node2D
 class_name Game
 
-signal level_complete;
+signal level_complete
+signal timer_end
 
 const START_GOAL = 1500
 const GOAL_INCREMENT = 2000
 var goal = 5000
 
 var level = 1
-var timer_value = 0
+var timer_value = 60
 
 var total_score = 0
 
@@ -25,3 +26,10 @@ func _on_board_match_found(score):
 	if total_score >= goal:
 		level_complete.emit()
 
+func _on_timer_timeout():
+	timer_value -= 1
+	if timer_value == 0:
+		timer_end.emit()
+		$Timer.stop()
+	
+	$GameStatus.set_timer(timer_value)

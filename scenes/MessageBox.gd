2bcd98e71745b4ac36f87cdbf302b2799203d955
Mirror83 <glenochieng045@gmail.com>
2024-08-21 06:40:14 +0300

    feat: added message box
    The message box can be used to show short messages for scenarios such as level completion and game over

---
+++ scenes/MessageBox.gd
@@ -0,0 +1,31 @@
+extends Control
+class_name MessageBox
+
+
+var message: String;
+
+func set_message(msg: String):
+	message = msg
+
+func _ready():
+	$ColorRect/Label.text = message
+	var screen_size = get_viewport_rect().size
+	$ColorRect.visible = false
+	await get_tree().create_timer(1).timeout
+	$ColorRect.visible = true
+	
+	var tween = create_tween()
+	tween.tween_property(
+		$ColorRect, "position",
+		Vector2(
+			$ColorRect.position.x,
+			screen_size.y / 2 - $ColorRect.get_rect().size.y / 2
+		),
+		0.5
+	)
+	tween.tween_property(
+		$ColorRect, "position",
+		Vector2($ColorRect.position.x, screen_size.y),
+		0.5
+	).set_delay(2)
+	tween.tween_callback(func(): queue_free())
